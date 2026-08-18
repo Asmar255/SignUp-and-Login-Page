7d813342360ada16f7e08230209aeb0991983ec8
Asmar255 <asmarnaeem255@gmail.com>
2026-08-18 16:30:22 +0500

    done creating the main heading

--- login-signup-project/app/(tabs)/Login.tsx
+++ login-signup-project/app/(tabs)/Login.tsx
@@ -1,14 +1,57 @@
-import { Text, View } from 'react-native'
+import { StyleSheet,Text, View, TextInput } from 'react-native'
 import React, { Component } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export class Login extends Component {
   render() {
     return (
-      <View>
-        <Text>Login</Text>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.textContainer}>
+          <Text style={styles.Title}>Welcome</Text>
+          <Text style={styles.subTitle}>Sign in to continue</Text>
+        </View>
+        <View style={styles.card}>
+            <Text style={styles.default}>Email</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="hello@example.com"
+              placeholderTextColor="#888"
+            />
+        </View>
+      </SafeAreaView>
     )
   }
 }
-
+const styles=StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:'#d2d7e0',
+    justifyContent: 'center',   
+    alignItems: 'center',
+  },
+  textContainer: {
+    alignItems: 'center',    
+  },
+  Title:{
+    fontSize:27,
+    fontWeight:'bold',
+  },
+  subTitle:{
+    fontSize:18,
+    color:'#677795',
+  },
+  default:{
+    color:'#0b0000'
+  },
+  card:{
+    width:'auto',
+    backgroundColor: '#FFFFFF',
+  },
+  input:{
+    borderColor:'#E2E8F0',
+    borderRadius:8,
+    borderWidth:1,
+    paddingHorizontal:10,
+  }
+})
 export default Login
--- login-signup-project/app/(tabs)/_layout.tsx
+++ login-signup-project/app/(tabs)/_layout.tsx
@@ -19,14 +19,14 @@ export default function TabLayout() {
       <Tabs.Screen
         name="Login"
         options={{
-          title: 'Home',
+          title: 'Login',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
       <Tabs.Screen
         name="SignUp"
         options={{
-          title: 'Explore',
+          title: 'SignUp',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
