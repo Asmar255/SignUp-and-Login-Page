03e04d63a62c5b39f5341829e9ad9aa697adaa22
Asmar255 <asmarnaeem255@gmail.com>
2026-08-19 22:29:01 +0500

    completed the SignUp and LoginPage frontend

--- login-signup-project/app/(tabs)/SignUp.tsx
+++ login-signup-project/app/(tabs)/SignUp.tsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import AntDesign from '@expo/vector-icons/AntDesign';
 
+
 export class SignUp extends Component {
   render() {
     return (
@@ -26,14 +27,54 @@ export class SignUp extends Component {
           
           {/* input feilds section*/}
           <View>
-            <Text>Full Name</Text>
+            <Text style={styles.InputText}>Full Name</Text>
             <TextInput
               style={styles.InputFeilds}
               placeholder='Henry Diaz'
               placeholderTextColor="#888"
             />
+
+            <Text style={styles.InputText}>Email Address</Text>
+            <TextInput
+              style={styles.InputFeilds}
+              placeholder='name@example.com'
+              placeholderTextColor="#888"
+            />
+
+            <Text style={styles.InputText}>Passward</Text>
+            <View style={styles.inputFlex}>
+              <TextInput
+              style={styles.InputFeilds}
+              placeholder='••••••••'
+              placeholderTextColor="#888"
+            />
+            <Pressable style={styles.eyeicons}>
+                <AntDesign name="eye" size={24} color="black " />
+            </Pressable>
+            </View>
           </View>
-          
+
+          {/* Signup button */}
+          <View>
+            <Pressable 
+            style={({pressed})=>[styles.Signupbtn, pressed && {opacity:0.6 } ]}
+            >
+                <Text style={styles.textbtn}>Sign Up</Text>
+            </Pressable>
+          </View>
+
+          {/* OR Line*/}
+          <View style={styles.dividerContainer}>
+            <View style={styles.line} />
+            <Text style={styles.dividerText}>or</Text>
+            <View style={styles.line} />
+          </View>
+
+          {/* Login line */}
+          <View style={styles.flexLast}>
+            <Text>Already Have a Account.</Text>
+            <Text style={styles.textlast}>Login</Text>
+          </View>
         </View>
       </SafeAreaView>
     )
@@ -82,11 +123,70 @@ const styles=StyleSheet.create({
       margin:'auto',
       marginTop:1,
     },
+    // card inputs
     InputFeilds:{
-      borderColor:'#E2E8F0',
+    borderColor:'#E2E8F0',
     borderRadius:8,
     borderWidth:1,
     padding:13,
+    width:'100%'
     },
+    InputText:{
+      fontWeight:'bold',
+      marginTop:25,
+      marginBottom:10,
+    },
+    inputFlex:{
+      flex:1,
+      flexDirection:'row',
+      alignItems:'center',
+    },
+    eyeicons:{
+      marginLeft:20,
+    },
+
+    // Signup BUtton
+    Signupbtn:{
+    marginTop:30,
+    backgroundColor:'#3B30E8',
+    borderRadius:8,
+    borderWidth:1,
+    padding:13,
+    width:'100%'
+    },
+    textbtn:{
+      color:'#fff',
+      margin:'auto',
+      fontSize:20,
+    },
+
+    // Or Line
+    dividerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 20,
+    },
+    line: {
+      flex: 1,                
+      height: 1,
+      backgroundColor: '#E2E8F0',
+    },
+    dividerText: {
+      marginHorizontal: 12,    
+      color: '#888888',
+      fontSize: 14,
+    },
+
+    // Login Line
+    flexLast:{
+    marginTop:20,
+    flexDirection:'row',
+    gap:8,
+    alignItems:'center',
+    justifyContent:'center'
+    },
+    textlast:{
+      color:'#4169E1',
+    }
 })
 export default SignUp
