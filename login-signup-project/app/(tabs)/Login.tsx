8277187887d87e8f80724d08b94eab5e17ac1f4d
Asmar255 <asmarnaeem255@gmail.com>
2026-08-19 11:07:07 +0500

    done craeting the frontend of login page

--- login-signup-project/app/(tabs)/Login.tsx
+++ login-signup-project/app/(tabs)/Login.tsx
@@ -1,15 +1,18 @@
 import { StyleSheet,Text, View, TextInput, Pressable, Alert } from 'react-native'
 import React, { Component } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 export class Login extends Component {
   render() {
     return (
       <SafeAreaView style={styles.container}>
+        {/* main heading */}
         <View style={styles.textContainer}>
           <Text style={styles.Title}>Welcome</Text>
           <Text style={styles.subTitle}>Sign in to continue</Text>
         </View>
+        {/* card section */}
         <View style={styles.card}>
             <Text style={styles.default}>Email</Text>
             <TextInput
@@ -36,11 +39,24 @@ export class Login extends Component {
               <Text style={styles.dividerText}>or</Text>
               <View style={styles.line} />
             </View>
-            <View>
-              <View></View>
-              <View></View>
+
+            {/* ggole and apple buttons */}
+            <View style={styles.btnflex}>
+              <View style={[styles.flex,styles.btnBottom]}>
+                <AntDesign name="google" size={24} color="black" />
+                <Text>Google</Text>
+              </View>
+              <View style={[styles.flex,styles.btnBottom]}>
+                <AntDesign name="apple" size={24} color="black" />
+                <Text>Apple</Text>
+              </View>
             </View>
         </View>
+         {/* last line */}
+            <View style={styles.flexLast}>
+              <Text>Don't have an account?</Text>
+              <Text style={styles.textlast}>Sign up</Text>
+            </View>
       </SafeAreaView>
     )
   }
@@ -115,5 +131,32 @@ const styles=StyleSheet.create({
     color: '#888888',
     fontSize: 14,
   },
+  //bottom buttons
+  flex:{
+    flexDirection:'row',
+    gap:10,
+    alignItems:'center'
+  },
+  btnflex:{
+    flexDirection:'row',
+    justifyContent:'center',
+    gap:100
+  },
+  btnBottom:{
+    borderColor:'#E2E8F0',
+    borderRadius:10,
+    borderWidth:2,
+    padding:8,
+  },
+  flexLast:{
+    marginTop:20,
+    flexDirection:'row',
+    gap:8,
+    alignItems:'center',
+    justifyContent:'center'
+  },
+  textlast:{
+    color:'#4169E1',
+  }
 })
 export default Login
