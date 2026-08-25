15d83f279babd3c0d004dfeecc60327f9f513769
Asmar255 <asmarnaeem255@gmail.com>
2026-08-25 12:16:48 +0500

    adding the auth file

---
+++ login-signup-project/components/auth.tsx
@@ -0,0 +1,10 @@
+// initial data 
+export const Mock_User=[
+    {name:'Asmar Naeem',email:'asmarnaeem@gmail.com',passward:'123456'},
+    {name:'mukarram raeel',email:'mukarramraeel@gmail.com',passward:'567890'}
+]
+
+//Login fucntion
+export const loginUser=(email:string,passward:string)=>{
+                
+}
